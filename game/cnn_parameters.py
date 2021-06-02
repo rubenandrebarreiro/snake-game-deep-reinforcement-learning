700fd845e29f584e587e5f78da9a3a0d667ad3b0
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-06-02 18:40:06 +0100

    Some minor and improvements changes added to the Parameters of the CNN

--- game/cnn_parameters.py
+++ game/cnn_parameters.py
@@ -1,5 +1,35 @@
-OPTIMISERS = ["ADAM"]
+"""
 
-KERNEL_SIZES = [8, 4]
+Deep Learning - Assignment #2:
+- Snake Game - Deep Reinforcement Learning
 
-STRIDES = [4, 2]
+Integrated Master of Computer Science and Engineering
+
+NOVA School of Science and Technology,
+NOVA University of Lisbon - 2020/2021
+
+Authors:
+- Rodrigo Jorge Ribeiro (rj.ribeiro@campus.fct.unl.pt)
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+Instructor(s):
+- Ludwig Krippahl (ludi@fct.unl.pt)
+- Claudia Soares (claudia.soares@fct.unl.pt)
+
+Snake Agent Module for the the Project
+
+"""
+
+# Constants and Flags
+
+# The List of Optimisers to use,
+# in the CNN (Convolutional Neural Network)
+OPTIMISERS_LIST = ["ADAM"]
+
+# The List of Kernel Sizes to use,
+# in the CNN (Convolutional Neural Network)
+KERNEL_SIZES_LIST = [8, 4]
+
+# The List of Strides to use,
+# in the CNN (Convolutional Neural Network)
+STRIDES_LIST = [4, 2]
