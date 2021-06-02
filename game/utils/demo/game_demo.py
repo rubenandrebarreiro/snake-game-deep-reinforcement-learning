576c6c4168f59578b8d965fb74c2c663c679d56b
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-06-02 14:25:20 +0100

    Some refactoring done

--- game/utils/demo/game_demo.py
+++ game/utils/demo/game_demo.py
@@ -11,13 +11,14 @@ NOVA School of Science and Technology,
 NOVA University of Lisbon - 2020/2021
 
 Authors:
-- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt) - Student no. 42648
+- Rodrigo Jorge Ribeiro (rj.ribeiro@campus.fct.unl.pt)
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
 
 Instructor(s):
 - Ludwig Krippahl (ludi@fct.unl.pt)
 - Claudia Soares (claudia.soares@fct.unl.pt)
 
-Snake Game Demo
+Snake Game Demonstration
 (given by the Instructor(s))
 
 """
@@ -28,7 +29,7 @@ Snake Game Demo
 import matplotlib.pyplot as py_plot
 
 # Import the Snake Game, with the Snake_Game alias
-from snake_game import SnakeGame as Snake_Game
+from game.snake_game import SnakeGame as Snake_Game
 
 
 # Function to plot the Board of the Snake Game
@@ -69,7 +70,7 @@ def snake_demo(actions):
     action_name = {-1: "Turn Left", 0: "Straight Ahead", 1: "Turn Right"}
 
     # Plot the Board of the Snake Game for the initial state
-    plot_board("0.png", board, "Start")
+    plot_board("images/0.png", board, "Start")
 
     # For each Frame and Action taken
     for frame, action in enumerate(actions):
@@ -78,7 +79,7 @@ def snake_demo(actions):
         board, reward, done, info = snake_game.step(action=action)
 
         # Plot the Board of the Game for the Action taken
-        plot_board(f"{(frame + 1)}.png", board, action_name[action])
+        plot_board(f"images/{(frame + 1)}.png", board, action_name[action])
 
 
 # Initialise the Demo for the Snake Game
