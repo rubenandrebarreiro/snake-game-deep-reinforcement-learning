4a26caff75ee03cf9477c69ac8185257317234b4
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-06-06 00:59:20 +0100

    More parameters added

--- game/others/parameters_arguments.py
+++ game/others/parameters_arguments.py
@@ -53,6 +53,9 @@ SLEEP_TIME_SECS = 0
 # The Maximum Memory capacity for the Snake Agent
 MAX_MEMORY = 100000
 
+# The Number of Games (Training Episodes)
+NUM_GAME_TRAINING_EPISODES = 30000
+
 # The initial value for the Epsilon variable for the Randomness used to,
 # decide about Exploration and Exploitation
 INITIAL_EPSILON_RANDOMNESS = 1
@@ -63,21 +66,18 @@ MAXIMUM_EPSILON_RANDOMNESS = 1
 
 # The Minimum value for the Epsilon variable for the Randomness used to,
 # decide about Exploration and Exploitation
-MINIMUM_EPSILON_RANDOMNESS = 0.01
+MINIMUM_EPSILON_RANDOMNESS = 0.1
 
 # The Decay Factor to adjust the value for
 # the Epsilon variable for the Randomness used to,
 # decide about Exploration and Exploitation
-DECAY_FACTOR_EPSILON_RANDOMNESS = 0.01
+DECAY_FACTOR_EPSILON_RANDOMNESS = ((INITIAL_EPSILON_RANDOMNESS - MINIMUM_EPSILON_RANDOMNESS) / NUM_GAME_TRAINING_EPISODES * 0.5)
 
 # The Gamma (Discount Reward) for the Q-Learning Algorithm
-GAMMA_DISCOUNT_FACTOR = 0.9
-
-# The Number of Games (Training Episodes)
-NUM_GAME_TRAINING_EPISODES = 300
+GAMMA_DISCOUNT_FACTOR = 0.95
 
 # The size of the Batch of Examples of Observations
-BATCH_SIZE = 1000
+BATCH_SIZE = 50
 
 # The List of Optimisers available to use for
 # the CNN (Convolutional Neural Network) Model
